ee71c87526f79c42f0c8d68d310898e68bff513b
JFFleming <84772413+JFFleming@users.noreply.github.com>
2024-08-15 16:09:54 +0900

    Update DEScoreCalculator.pl

--- BOSTIn_v1.2/DEScoreCalculator.pl
+++ BOSTIn_v1.2/DEScoreCalculator.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use FAST::Bio::SeqIO;
 
+#Read the Fasta File, and open the output files for writing
 my $alignFile = FAST::Bio::SeqIO->new(-file => $ARGV[0], -format => 'Fasta', -alphabet => protein);
 my $FreqFile = "$ARGV[1].SiteSaturation.TotalFrequencies.txt";
 my $TaxFile = "$ARGV[1].SiteSaturation.TaxaFrequencies.txt";
@@ -10,6 +11,8 @@ open (FREQ, '>', $FreqFile) || die ("Can not open $FreqFile\n");
 print FREQ "FileName\tExchangeFreq\tExchangeFreqStDev\tDE-Score\n";
 open (TAXA, '>', $TaxFile) || die ("Can not open $TaxFile\n");
 print TAXA "FileName\tExchangeFreq\tExchangeFreqStDev\tDE-Score\n";
+
+#Set the Dayhoff Groups
 my @AllSeqs = ();
 my @Small= ("A","G","P","S","T");
 my @AcidAmide= ("D","E","N","Q");
@@ -19,6 +22,7 @@ my @Aromatic= ("F","W","Y");
 my @Sulfur= ("C");
 my %PhySeqs;
 
+#Read the sequences in, the split into sequence and taxa ID. Then split the sequence into individual characters and store these together in a hash.
 while ( my $seq = $alignFile->next_seq() ) {
 	my $ID = $seq->display_id;
 	my $subseq = $seq->subseq(1, $seq->length());
@@ -28,17 +32,21 @@ while ( my $seq = $alignFile->next_seq() ) {
 	$PhySeqs{$ID}=[@sequence];
 }
 
+#debugging
 #foreach my $key (keys %PhySeqs){
 #	my @unlock = @{$PhySeqs{$key}};
 #	print "$unlock[1]\n";
 #}
 
+#Print the number of taxa in the dataset
 my $size = keys %PhySeqs;
 print "there are $size keys in the hash\n";
 my $a = 0;
 my @AllTvs;
 my @AllTis;
 my @AllTiFreqs;
+
+#Foreach taxa in the dataset, make pairwise comparisons against each character in the sequence one by one, against each other taxa - compk
 
 foreach my $k (keys %PhySeqs){
 	my @vals = @{ $PhySeqs{$k}};
@@ -101,6 +109,7 @@ foreach my $k (keys %PhySeqs){
 #			print "TI: $pairwiseTI TV: $pairwiseTV \n";
 		my $TiFreq = $pairwiseTI/($pairwiseTI+$pairwiseTV);
 #		print "TI $pairwiseTI TV $pairwiseTV \t $SmallCounter \t $SameCounter \n";
+#Having looked at each taxa for a pairwise comparison, shove the values into a taxa specific array for later and an all values array for later.
 		push (@AllTis, $pairwiseTI);
 		push (@AllTvs, $pairwiseTV);
 		push (@AllTiFreqs, $TiFreq);
@@ -110,6 +119,7 @@ foreach my $k (keys %PhySeqs){
 		}
 	}
 	$a++;
+ #Calculate Taxa specific Between and Within Dayhoff Group exchange frequencies, standard deviations and the taxa specific DE-Score.
 	my $TaxAverageTransI = avg(\@TaxTis);
 	my $TaxAverageTransV = avg(\@TaxTvs);
 	my $TaxAverageTiFreq = avg(\@TaxTiFreqs);
@@ -123,6 +133,7 @@ foreach my $k (keys %PhySeqs){
 }
 #my $checksize = scalar(@AllTiFreqs);
 #print $checksize;
+#Calculate same values as above but for the whole dataset.
 my $AverageTransI = avg(\@AllTis);
 my $AverageTransV = avg(\@AllTvs);
 my $AverageTiFreq = avg(\@AllTiFreqs);
@@ -133,6 +144,7 @@ my $totalDE = $AverageTiFreq/($size**-0.08);
 
 print FREQ "$ARGV[0]\t$AverageTiFreq\t$StdTiFreq\t$totalDE\n";
 
+#Subroutine to calculate the average
 sub avg {
   my ($avg_array_ref) = @_;
   my $avg_count = @$avg_array_ref;
@@ -143,10 +155,12 @@ sub avg {
   return $avg_sum / $avg_count;
 }
 
+#Subroutine to calculate the standard deviation
 sub get_stddev {
   return sqrt(get_disp(@_));
 }
 
+#Subroutine to calculate the disparity for the standard deviation
 sub get_disp {
   my ($dis_array_ref) = @_;
   my $mean = avg($dis_array_ref);
