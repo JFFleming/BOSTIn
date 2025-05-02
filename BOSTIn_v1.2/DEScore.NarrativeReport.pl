0956fdc9a9ce73eb2ace182482fc041fa2c0e034
JFFleming <84772413+JFFleming@users.noreply.github.com>
2025-05-02 16:59:28 +0900

    Update DEScore.NarrativeReport.pl
    Complete rewrite in line with the new calculation for the DE-Score

--- BOSTIn_v1.2/DEScore.NarrativeReport.pl
+++ BOSTIn_v1.2/DEScore.NarrativeReport.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-
-#Take the output Prefix as the command line argument to parse downstream inputs and make outputs.
 my $prefix = $ARGV[0];
 my $summInput = "$prefix.SiteSaturation.TotalFrequencies.txt";
 open(my $summFile, '<', $summInput);
@@ -10,40 +8,38 @@ my $fulldummy = <$summFile>;
 
 my $totalDE;
 my $TIFreq;
-#Read the Summary file produced by the DE Score calculator and extract the relevant output values.
+
 while(<$summFile>){
 	my $summline = $_;
 	chomp($summline);
 	my @splitter = split(/\t/, $summline);
-	$totalDE = $splitter[4];
+	$totalDE = $splitter[3];
 	$TIFreq = $splitter[1];
 }
 close($summFile);
 
 print "DE-Score
 	As you are using amino acid data, we selected the DE-Score - the Dayhoff Exchange Score. This is based on the ratio of pairwise within Dayhoff Category changes and between Dayhoff Category changes throughout the alignment. It also has the advantage that it can be used to detect taxa that are particularly saturated. We call this the tDE-Score.
-	The DE-Score of this entire dataset is $totalDE, with a Dayhoff Category Exchange Ratio of $TIFreq. ";
+	The DE-Score of this entire dataset is $totalDE, with a Dayhoff Category Exchange Ratio of $TIFreq.
+	The DE-Score is the average of the taxon-specific DE-Scores.
+        When evaluating the total DE-Score, consider two things. First, a DE-Score below 0 indicates that the data is completely saturated, and likely uninformative - for best results, seek a DE-Score of 0.354 or higher. This means that the alignment is 2 minimum-information steps away from total saturation (2x0.177 away from DE-Score 0). When comparing between alignments, the one with a higher DE-Score has less entropic site saturation.";
 
-if ($totalDE > 0.5){
- print "As the DE-Score is very high, this means that saturation is unlikely to be a problem with this dataset.\n";
+if ($totalDE > 0.353){
+ print "As the overall DE-Score is higher than 0.354, this means that saturation is unlikely to be a problem with this dataset overall, but pockets of entropic saturation may still be an issue. Look at the tDE-Scores to check.\n";
 }
-elsif ($totalDE > 0.3){
-	print "As the DE-Score is above 0.3 but below 0.5, it means that this dataset is not entirely saturated, but as it is still quite low, so you might want to observe the distribution of the tDE-Scores, to see if any taxa are contributing particularly to the score.\n";
+elsif ($totalDE < 0.354){
+	print "As the DE-Score is less than 0.354, saturation might be a serious problem in this dataset. Check the tDE-Scores to see which taxa are contributing to this.\n";
 }
-else{
-	print "As the DE-Score is below 0.3, this means that saturation might be a serious problem in this dataset. Check the tDE-Scores to see which taxa are contributing to this.\n";
-}
-#Open the R Summary file and extract the relevant taxon specific files from the R Summary output.
+
 my $t_summary = "$prefix.SiteSaturation.TaxaFrequencies.summary.txt";
 my @t_stats = summaryStats($t_summary);
 
 my $tDE_mean = $t_stats[0];
 my $tDE_median = $t_stats[1];
-my $tDE_sd = $t_stats[2]; 
+my $tDE_sd = $t_stats[2];
 my $tDE_lowsig = $tDE_mean-(2*$tDE_sd);
 my $tDE_highsig = $tDE_mean+(2*$tDE_sd);
 
-#Use the Red Lorry Yellow Lorry subroutine to extract the Red Flag and Yellow Flag taxa from the txt files produced by the R script.
 my $red_tfile = "$prefix.SiteSaturation.TaxaFrequencies.SiteSat.Taxa.redflags.txt";
 my @tDE_red = redLorryYellow($red_tfile);
 
@@ -54,9 +50,11 @@ print "tDE-Score
 	A histogram of tDE-Scores can be seen here:
 	
 	$prefix.SiteSaturation.TaxaFrequencies.histogram.pdf
-	
-	The difference between the median, $tDE_median ,  and the mean, $tDE_mean , can tell us about the distribution of tDE-Scores, and help us identify taxa that are particularly prone to causing topological artifacts due to site saturation.";
-
+        For further analysis, the histograms show the distribution of taxon-specific DE-Scores across the taxa in your dataset, which can help you work out which taxa might end up causing problems in your final phylogenetic analysis.
+        We can use the median and the mean of the tDE-Score to better understand what the distribution of site saturation in your dataset looks like.
+        tDE-Score median: $tDE_median
+	tDE-Score mean: $tDE_mean
+";
 
 if ($tDE_median > $tDE_highsig){	
 	print "The mean is significantly smaller than the median, which suggests that some sequences in the dataset are very significantly saturated. Explore the histogram to learn more, and consider removing the red flag taxa.";
@@ -73,7 +71,7 @@ elsif ($tDE_median < $tDE_mean){
 else{
 	print "Your median and mean are equivalent, which suggests that saturation is well distributed across your dataset.";
 }
-print "From the median, we can identify potentially problematic sequences by observing how many standard deviations and median absolute deviations an individual tDE-Score is from the median. Red flag taxa are more than 2 standard deviations from the median, while Yellow Flag taxa are more than 2 median absolute deviations from the median.
+print "Beyond this, we can identify potentially problematic sequences by using the same method as for the whole dataset. We can observe which specific taxa have DE-Scores below 0. We mark these as highly saturated Red Flag taxa. We can also mark taxa with DE-Scores below 0.354 as slightly saturated Yellow Flags.
 	The Red Flag taxa are:
 	@tDE_red
 	The Yellow Flag taxa are:
