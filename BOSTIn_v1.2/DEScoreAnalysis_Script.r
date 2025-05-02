763f4bdc8d42b285b66029d9b545a1c602dfc9c5
JFFleming <84772413+JFFleming@users.noreply.github.com>
2025-05-02 16:58:52 +0900

    Update DEScoreAnalysis_Script.r
    Complete rewrite to align with the new calculation of the DE-Score

--- BOSTIn_v1.2/DEScoreAnalysis_Script.r
+++ BOSTIn_v1.2/DEScoreAnalysis_Script.r
@@ -4,41 +4,39 @@ t_filename <- args[1]
 tDEdata <- read.table(t_filename, skip=1)
 t_pdf <- paste0(tools::file_path_sans_ext(t_filename),".histogram.pdf")
 pdf(file = t_pdf, width=4, height=4)
-hist(tDEdata$V5, main="Histogram of t-DE Score values", xlab="t-DE Score Values")
+hist(tDEdata$V4, main="Histogram of t-DE Score values", xlab="t-DE Score Values")
 dev.off()
-#This section creates the summary file, then calculates the mean, median, standard deviation and median absolute deviation of the taxon specific DE Scores.
+
 t_summ_file <- paste0(tools::file_path_sans_ext(t_filename),".summary.txt")
 t_summ_list <- c()
 t_summ_list <- append(t_summ_list, "t-DE summary statistics\n")
-t_mean = mean(tDEdata$V5)
+t_mean = mean(tDEdata$V4)
 t_summ_list <- append(t_summ_list, paste("mean\t", t_mean, "\n"))
-t_median = median(tDEdata$V5)
+t_median = median(tDEdata$V4)
 t_summ_list <- append(t_summ_list, paste("median\t", t_median, "\n"))
-t_sd = sd(tDEdata$V5)
+t_sd = sd(tDEdata$V4)
 t_summ_list <- append(t_summ_list, paste("standard deviation\t", t_sd, "\n"))
-t_mad = mad(tDEdata$V5)
+t_mad = mad(tDEdata$V4)
 t_summ_list <- append(t_summ_list, paste("median absolute deviation\t", t_mad, "\n"))
 cat(t_summ_list, file=t_summ_file)
 
-#Based on the MAD from the median, this identifies red flag taxa and places them in the output txt file.
 redfile <- paste0(tools::file_path_sans_ext(t_filename),".SiteSat.Taxa.redflags.txt")
 t_RedFlags<-file(redfile)
 redList <- c()
-for(i in 1:length(tDEdata$V5)){
-	if(tDEdata$V5[i] < (median(tDEdata$V5)-(2*mad(tDEdata$V5)))){
-		redTaxa <- paste(tDEdata$V1[i], "\t", tDEdata$V5[i], "\n")
+for(i in 1:length(tDEdata$V4)){
+	if(tDEdata$V4[i] < 0){
+		redTaxa <- paste(tDEdata$V1[i], "\t", tDEdata$V4[i], "\n")
 		redList<- append(redList, redTaxa)
 	}
 }
 cat(redList, file=t_RedFlags)
 
-#Based on the standard deviation from the median, this identifies yellow flag taxa and places them in the output txt file.
 yellowfile <- paste0(tools::file_path_sans_ext(t_filename),".SiteSat.Taxa.yellowflags.txt")
 t_YellowFlags<-file(yellowfile)
 yellowList <- c()
-for(i in 1:length(tDEdata$V5)){
-	if(tDEdata$V5[i] < (median(tDEdata$V5)-(2*sd(tDEdata$V5)))){
-		yellowTaxa <- paste(tDEdata$V1[i], "\t", tDEdata$V5[i], "\n")
+for(i in 1:length(tDEdata$V4)){
+	if(tDEdata$V4[i] < 0.354){
+		yellowTaxa <- paste(tDEdata$V1[i], "\t", tDEdata$V4[i], "\n")
 		yellowList<- append(yellowList, yellowTaxa)
 	}
 }
