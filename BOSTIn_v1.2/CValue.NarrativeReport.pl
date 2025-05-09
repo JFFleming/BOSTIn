9080f1673224651fd7788c163c24fd15ebf7feab
JFFleming <84772413+JFFleming@users.noreply.github.com>
2025-05-10 00:15:24 +0900

    Update CValue.NarrativeReport.pl
    Fixing Output File Notation

--- BOSTIn_v1.2/CValue.NarrativeReport.pl
+++ BOSTIn_v1.2/CValue.NarrativeReport.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 my $prefix = $ARGV[0];
-my $summInput = "$prefix.SiteSaturation.TotalCValue.txt";
+my $summInput = "$prefix.SiteSaturation.TotalCScore.txt";
 open(my $summFile, '<', $summInput);
 my $fulldummy = <$summFile>;
 
@@ -17,21 +17,21 @@ while(<$summFile>){
 }
 close($summFile);
 
-print "C Factor
-	As you are using nucleotide data, we selected the C Factor - the Convergence Factor. This is based on the ratio of the standard deviation of the Transition/Transversion ratio of the dataset and the standard deviation of the uncorrected p-distance. It also has the advantage that it can be used to detect taxa that are particularly saturated. We call this the taxon C Factor.
-	The C Factor of this entire dataset is $stats{CValue}, where the standard deviation of the transition transversion ratios is $stats{StdDevTiTv} and the standard deviation of the uncorrected p-distances is $stats{PValue}.";
+print "C Score
+	As you are using nucleotide data, we selected the C Score - the Convergence Factor. This is based on the ratio of the standard deviation of the Transition/Transversion ratio of the dataset and the standard deviation of the uncorrected p-distance. It also has the advantage that it can be used to detect taxa that are particularly saturated. We call this the taxon C Score.
+	The C Score of this entire dataset is $stats{CScore}, where the standard deviation of the transition transversion ratios is $stats{StdDevTiTv} and the standard deviation of the uncorrected p-distances is $stats{PValue}.";
 
-if ($stats{CValue} > 20){
- print "As the C Factor is greater than 20, this means that saturation is unlikely to be a problem with this dataset, as the Observed and Expected Ti/Tv ratio is likely greater than 1 (Struck et al 2008).\n";
+if ($stats{CScore} > 20){
+ print "As the C Score is greater than 20, this means that saturation is unlikely to be a problem with this dataset, as the Observed and Expected Ti/Tv ratio is likely greater than 1 (Struck et al 2008).\n";
 }
-elsif ($stats{CValue} > 10){
-	print "As the C Factor is between 10 and 20, it means that this dataset is not entirely saturated, but that saturation might affect topological reconstruction. As it is still quite low, you might want to observe the distribution of the taxa C Factors, to see if any taxa are contributing particularly to the overall score.\n";
+elsif ($stats{CScore} > 10){
+	print "As the C Score is between 10 and 20, it means that this dataset is not entirely saturated, but that saturation might affect topological reconstruction. As it is still quite low, you might want to observe the distribution of the taxa C Scores, to see if any taxa are contributing particularly to the overall score.\n";
 }
 else{
-	print "As the C Factor is below 10, this means that saturation might be a serious problem in this dataset. Check the taxa C Factors to see which taxa are contributing to this.\n";
+	print "As the C Score is below 10, this means that saturation might be a serious problem in this dataset. Check the taxa C Scores to see which taxa are contributing to this.\n";
 }
 
-my $t_summary = "$prefix.SiteSaturation.TaxaCValue.summary.txt";
+my $t_summary = "$prefix.SiteSaturation.TaxaCScore.txt";
 my @t_stats = summaryStats($t_summary);
 
 my $tC_mean = $t_stats[0];
@@ -42,24 +42,22 @@ my $tC_10 = $t_stats[5];
 my $tC_lowsig = $tC_mean-(2*$tC_sd);
 my $tC_highsig = $tC_mean+(2*$tC_sd);
 
-#print "$tC_mean $tC_median $tC_sd";
-
-my $red_tfile = "$prefix.SiteSaturation.TaxaCValue.redflags.txt";
+my $red_tfile = "$prefix.SiteSaturation.TaxaCScore.redflags.txt";
 my @tC_red = redLorryYellow($red_tfile);
 
-my $yellow_tfile = "$prefix.SiteSaturation.TaxaCValue.yellowflags.txt";
+my $yellow_tfile = "$prefix.SiteSaturation.TaxaCScore.yellowflags.txt";
 my @tC_yellow = redLorryYellow($yellow_tfile);
 
-print "Taxa C Factor
-	A histogram of taxa C Factors can be seen here:
+print "Taxa C Score
+	A histogram of taxa C Scores can be seen here:
 	
 	$prefix.SiteSaturation.TaxaFrequencies.histogram.pdf
 	
-	The C Factor is unique among the BOSTIn metrics. in that it has a more absolute way of assessing saturation. Sequences with a C Factor below 20 are potentially saturated to an uninformative degree, whereas taxa with a C Factor below 10 show significant amounts of saturation.
-	The Percentage of taxa in your dataset with a C Factor below 20 is $tC_20.
-	The Percentage of taxa in your dataset with a C Factor below 10 is $tC_10.\n";
+	The C Score is unique among the BOSTIn metrics. in that it has a more absolute way of assessing saturation. Sequences with a C Score below 20 are potentially saturated to an uninformative degree, whereas taxa with a C Score below 10 show significant amounts of saturation.
+	The Percentage of taxa in your dataset with a C Score below 20 is $tC_20.
+	The Percentage of taxa in your dataset with a C Score below 10 is $tC_10.\n";
 	
-print "From here, we can identify those potentially problematic sequences, assigning Yellow Flags to those with C Factors between 10 and 20, and Red Flags to those less than 10.
+print "From here, we can identify those potentially problematic sequences, assigning Yellow Flags to those with C Scores between 10 and 20, and Red Flags to those less than 10.
 	The Red Flag taxa are:
 	@tC_red
 	The Yellow Flag taxa are:
@@ -74,7 +72,6 @@ sub summaryStats {
 		my $line = $_;
 		my @splitSum = split(/\t/, $line);
 		my $stat = $splitSum[1];
-#		print $stat;
 		chomp($stat);
 		my $rounded = sprintf('%.5f', $stat);
 		push (@summary, $rounded);
